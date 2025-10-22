c914364967186aa807996d98d6d70ce0b77c7193
Silvia Lin Lü <ssll@kth.se>
2025-10-22 14:32:35 +0200

    Create corr-test

--- create-dataset.py
+++ create-dataset.py
@@ -4,19 +4,6 @@ import random
 import re
 import sys
 
-import zipfile
+### Corrupt one image
 
-zip_path = r"C:\Users\laira\Downloads\archive.zip"
-output_dir = r"C:\Users\laira\Documents\GitHub\dfir-bgpt"
-os.makedirs(output_dir, exist_ok=True)
 
-with zipfile.ZipFile(zip_path, 'r') as z:
-    all_files = z.namelist()  # All filenames in the zip
-    sample_files = random.sample(all_files, 5000)  # Pick 5k files
-
-    for i, f in enumerate(sample_files, 1):
-        z.extract(f, output_dir)
-        if i % 500 == 0:
-            print(f"Extracted {i} / 5000")
-
-print("5,000 images extracted to:", output_dir)
